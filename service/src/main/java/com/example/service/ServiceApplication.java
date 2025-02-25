8ecd91cfc73da0a378932fb73e62c165aa9be802
Josh Long <josh@joshlong.com>
2025-02-25 06:44:42 -0800

    now we have parity with 2024... need to introduce mcp goodness.

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -1,11 +1,17 @@
 package com.example.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -16,7 +22,17 @@ public class ServiceApplication {
 
     @Bean
     ChatClient chatClient(ChatClient.Builder builder  ,
+                          DogRepository repository,
                           VectorStore vectorStore) {
+
+        if (false) {
+            repository.findAll().forEach(dog -> {
+                var dogument = new Document("id: %s, name: %s, description: %s".formatted(
+                        dog.id(), dog.name(), dog.description()));
+                vectorStore.add(List.of(dogument));
+            });
+        }
+         
         
         var system = """
                 You are an AI powered assistant to help people adopt a dog from the adoption\s
@@ -27,16 +43,27 @@ public class ServiceApplication {
                 """;
         return builder
                 .defaultSystem(system)
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
     }
 
     @Bean
     ApplicationRunner assistantRunner(ChatClient cc) {
         return args -> {
-            var reply = cc.prompt("do you have any neurotic dogs?")
+            var reply = cc
+                    .prompt("do you have any neurotic dogs?")
                     .call()
-                    .content();
+                    .entity(DogAdoptionSuggestion.class);
             System.out.println("reply [" + reply + "]");
         };
     }
+}
+
+record Dog(@Id int id, String name, String description) {
+}
+
+interface DogRepository extends ListCrudRepository<Dog, Integer> {
+}
+
+record DogAdoptionSuggestion(String name, String description, int id) {
 }
